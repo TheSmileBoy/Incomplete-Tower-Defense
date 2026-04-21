6b47954730262c4624248d51deedfe93e53b31a0
Smile <temporariza@gmail.com>
2026-04-21 16:26:09 -0300

    Testing Azul

--- Incomplete TD/src/ReplicatedStorage/Services/NPCHandler/Resources/GetMap.lua
+++
@@ -1,9 +0,0 @@
-return function(NPCTable) :Folder?
-	local Map = game.Workspace:FindFirstChild(NPCTable.Map)
-	if not Map then 
-		warn("Map doesn't exist!", NPCTable.Map) 
-		return 
-	end
-	
-	return Map
-end
